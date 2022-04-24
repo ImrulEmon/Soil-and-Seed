c1499499c901138a7a0af64614d7da5072db82c7
ImrulEmon <emimrulemon@gmail.com>
2022-04-25 00:08:28 +0600

    :electron::seedling: services,details

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,20 +1,23 @@
 import React from "react";
+import './Details.css';
 import { NavLink, useParams } from "react-router-dom";
 import useServices from "../../hooks/useServices";
-
+import{FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowAltCircleLeft} from '@fortawesome/free-solid-svg-icons';
 const Details = () => {
   const { _id } = useParams();
   const [services, setServices] = useServices([]);
-  const tree = services.find((tree) => tree._id == _id);
+  const tree = services?.find((tree) => tree._id == _id);
   console.log(tree);
 //   const {name,img,info,price}=tree;
   return (
-    <div>
-      <h1>{services.length}</h1>
-        <h3>{tree?.name}</h3>
+    <div className="details text-justify container my-3">
+        <h3 className="title">{tree?.name}</h3>
+        <img className="w-50 mx-auto my-12 shadow-2xl" src={tree?.img} alt="" />
         <p>{tree?.info}</p>
-        <img src={tree?.img} alt="" />
-      <NavLink to="/services">Go Back to Services</NavLink>
+      <div className="text-center my-4 font-bold text-green-600">
+      <NavLink to="/services"> <FontAwesomeIcon icon={faArrowAltCircleLeft}/> Go Back to Services</NavLink>
+      </div>
     </div>
   );
 };
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Tree from "../Tree/Tree";
-
+import'./Services.css';
 const Services = () => {
   const [trees, setTrees] = useState([]);
   useEffect(() => {
@@ -10,8 +10,7 @@ const Services = () => {
   }, []);
   return (
     <div className="container mt-24">
-      <h1>Services</h1>
-      <h4>{trees.length}</h4>
+      <h2 className="text-6xl font-bold text-green-700 hover:text-green-800 bonsai italic">BONSAI</h2>
       <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
         {trees.map((tree) => (
           <Tree key={tree._id} tree={tree}></Tree>
